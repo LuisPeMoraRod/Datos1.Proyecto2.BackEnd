81041a738eca8d27360149f05dcd2e6f24ccd2a8
Luis Pedro Morales <luismorarod98@estudiantec.cr>
2020-07-10 15:35:18 -0600

    Add MyMenu doubly linked list used, bubblesort and sorted insertion

--- src/com/Project2/BackEnd/Trees/Node.java
+++ src/com/Project2/BackEnd/Trees/Node.java
@@ -2,29 +2,59 @@ package com.Project2.BackEnd.Trees;
 
 public class Node<T extends Comparable<T>> {
 
-    T element;
-    Node<T> left;
-    Node<T> right;
+	T element;
+	Node<T> left;
+	Node<T> right;
+	int id;
 
-    public Node(T element){
-        this(element, null, null);
-    }
+	/**
+	 * Constructs the node with its children
+	 * 
+	 * @param element
+	 * @param left
+	 * @param right
+	 */
 
-    /**
-     * Constructs the node with its children
-     * @param element
-     * @param left
-     * @param right
-     */
+	public Node(T element, Node<T> left, Node<T> right) {
+		this.element = element;
+		this.left = left;
+		this.right = right;
+	}
 
-    public Node(T element, Node<T> left, Node<T> right){
-        this.element = element;
-        this.left = left;
-        this.right = right;
-    }
-    
-    public T getElement() {
-    	return this.element;
-    }
+	public Node<T> getLeft() {
+		return left;
+	}
+
+	public void setLeft(Node<T> left) {
+		this.left = left;
+	}
+
+	public Node<T> getRight() {
+		return right;
+	}
+
+	public void setRight(Node<T> right) {
+		this.right = right;
+	}
+
+	public Node(T element) {
+		this(element, null, null);
+	}
+
+	public T getElement() {
+		return this.element;
+	}
+
+	public void setElement(T element) {
+		this.element = element;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
 
 }
