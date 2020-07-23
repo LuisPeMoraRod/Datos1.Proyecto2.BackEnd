5f9bc788921a57514a8464bafd7f6a8e763f2349
Luis Pedro Morales <luismorarod98@estudiantec.cr>
2020-07-23 00:39:08 -0600

    Add messageType attribute to notifications

--- src/com/Project2/BackEnd/REST/Notification.java
+++ src/com/Project2/BackEnd/REST/Notification.java
@@ -8,6 +8,7 @@ public class Notification {
 	private int notifType;
 	private String newComment;
 	private String recipe;
+	private String messageType;
 
 	public String getEmisorUser() {
 		return emisorUser;
@@ -49,12 +50,45 @@ public class Notification {
 		this.recipe = recipe;
 	}
 
+	public String getMessageType() {
+		return messageType;
+	}
+
+	public void setMessageType(int notifType) {
+		switch (notifType) {
+		case NEW_COMMENT:
+			this.messageType = " commented a recipe.";
+			break;
+		case NEW_FOLLOWER:
+			this.messageType = " followed you.";
+			break;
+		case NEW_UNFOLLLOW:
+			this.messageType = " unfollowed you.";
+			break;
+		case NEW_LIKE:
+			this.messageType = " liked a recipe.";
+			break;
+		case NEW_UNLIKE:
+			this.messageType = " disliked a recipe.";
+			break;
+		case NEW_SHARE:
+			this.messageType = " shared a recipe.";
+			break;
+		case NEW_UNSHARE:
+			this.messageType = " unshared a recipe.";
+			break;
+		default:
+			break;
+		}
+	}
+
 	public Notification(String emisorUser, String observerUser, int notifType, String newComment, String recipe) {
 		this.emisorUser = emisorUser;
 		this.recieverUser = observerUser;
 		this.notifType = notifType;
 		this.newComment = newComment;
 		this.recipe = recipe;
+		setMessageType(notifType);
 	}
 
 }
